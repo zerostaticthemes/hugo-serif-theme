4abfc966ff5459041863d941682fe516d7af98cc
Austin, Robert <rob@jugglerdigital.com>
2024-10-30 14:49:52 +1000

    only run sticky header script if enabled, avoid running when undefined

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,20 +1,22 @@
-var body = document.querySelector('body')
-var menuTrigger = document.querySelector('#toggle-main-menu-mobile');
-var menuContainer = document.querySelector('#main-menu-mobile');
+var body = document.querySelector("body");
+var menuTrigger = document.querySelector("#toggle-main-menu-mobile");
+var menuContainer = document.querySelector("#main-menu-mobile");
 
-menuTrigger.onclick = function() {
-    menuContainer.classList.toggle('open');
-    menuTrigger.classList.toggle('is-active')
-    body.classList.toggle('lock-scroll')
-}
+menuTrigger.onclick = function () {
+  menuContainer.classList.toggle("open");
+  menuTrigger.classList.toggle("is-active");
+  body.classList.toggle("lock-scroll");
+};
 
-window.onscroll = function() {makesticky()};
 var header = document.getElementById("stickyHeader");
-var sticky = header.offsetTop;
-function makesticky() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
-  }
-} 
+
+if (header) {
+  var offset = header.offsetTop;
+  document.addEventListener("scroll", function () {
+    if (window.scrollY > offset) {
+      header.classList.add("sticky");
+    } else {
+      header.classList.remove("sticky");
+    }
+  });
+}
